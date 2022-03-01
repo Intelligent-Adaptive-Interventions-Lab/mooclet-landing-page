4cc45f4914902ec4f5e30b3dcffcc2d2b0edfffe
Harsh Kumar <hope@Harshs-MacBook-Pro-2.local>
2022-03-01 10:46:08 -0500

    revisions

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,7 +5,7 @@ export const Team = (props) => {
         <div className='col-md-8 col-md-offset-2 section-title'>
           <h2>Meet the Team</h2>
           <p>
-            People behind building, developing and maintaining the Mooclet engine.
+            People behind building, developing and maintaining the MOOClet engine.
           </p>
         </div>
         <div id='row'>
@@ -14,7 +14,7 @@ export const Team = (props) => {
                 <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
                   <div className='thumbnail'>
                     {' '}
-                    <img src={d.img} alt='...' className='team-img' />
+                    <img src={d.img} alt='...' className='team-img' width='240px' height='240px' />
                     <div className='caption'>
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
