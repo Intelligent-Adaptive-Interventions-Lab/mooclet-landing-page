4442035cb40f6b6ee4fb94fbeebf85fcfd0d46bc
Harsh Kumar <hope@Harshs-MacBook-Pro-2.local>
2022-04-12 15:11:17 -0400

    added contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -20,7 +20,7 @@ export const Contact = (props) => {
     console.log(name, email, message)
     emailjs
       .sendForm(
-        'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
+        'service_a2l78c1', 'template_37ctbw9', e.target, 'YOUR_USER_ID'
       )
       .then(
         (result) => {
